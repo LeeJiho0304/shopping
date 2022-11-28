525b0cf12698f5df4c48cb0094e2795fe475cbd3
dlgld3 <dlgld3@naver.com>
2022-11-28 21:33:14 +0900

    연희 프로덕트

--- src/controller/product/ProductDetailController.java
+++ src/controller/product/ProductDetailController.java
@@ -9,23 +9,23 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name="ProductListController", urlPatterns="/ProductListController")
-public class ProductListController extends HttpServlet {
+@WebServlet(name="ProductDetailController", urlPatterns="/ProductDetailController")
+public class ProductDetailController extends HttpServlet {
 	@Override
 	public void init(ServletConfig config) throws ServletException {
-		System.out.println("ProductListController.init() 실행");
+		System.out.println("ProductDetailController.init() 실행");
 	}
 	
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("ProductListController.service() 실행");
+		System.out.println("ProductDetailController.service() 실행");
 		
 		//JSP로 이동
-		request.getRequestDispatcher("/WEB-INF/views/homePage/product/productList.jsp").forward(request, response);
+		request.getRequestDispatcher("/WEB-INF/views/homePage/product/productDetail.jsp").forward(request, response);
 	}
 	
 	@Override
 	public void destroy() {
-		System.out.println("ProductListController.destroy()");
+		System.out.println("ProductDetailController.destroy()");
 	}
 }
