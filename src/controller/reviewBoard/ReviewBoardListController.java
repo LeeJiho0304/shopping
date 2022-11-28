d7691d6fb67aecf8c603b499ea00231af5a75d9c
yerim96 <chzh9993@naver.com>
2022-11-28 21:37:18 +0900

    예림 리뷰리스트 컨트롤러 추가

--- src/controller/reviewBoard/ReviewBoardListController.java
+++ src/controller/reviewBoard/ReviewBoardListController.java
@@ -1,5 +1,32 @@
 package controller.reviewBoard;
 
-public class ReviewBoardListController {
+import java.io.IOException;
 
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name="ReviewBoardListController", urlPatterns="/ReviewBoardListController")
+public class ReviewBoardListController extends HttpServlet {
+	
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("ReviewBoardListController.init() 실행");
+	}
+	
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("ReviewBoardListController.service() 실행");
+		
+		//JSP로 이동
+		request.getRequestDispatcher("/WEB-INF/views/homePage/reviewBoard/reviewBoardList.jsp").forward(request, response);
+	}
+	
+	@Override
+	public void destroy() {
+		System.out.println("ReviewBoardListController.destroy()");
+	}
 }
