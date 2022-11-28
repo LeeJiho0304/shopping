f1b37e64d17e69e98e6dd8b273d02721901f1f90
LemonGlacier <hijy02122@gmail.com>
2022-11-28 21:35:28 +0900

    20221128 qna board controller

--- src/controller/qnaBoard/QnaBoardDetailContoller.java
+++ src/controller/qnaBoard/QnaBoardDetailContoller.java
@@ -9,25 +9,25 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name="QnaBoardListContoller", urlPatterns="/QnaBoardListContoller")
-public class QnaBoardListContoller extends HttpServlet {
+@WebServlet(name="QnaBoardDetailContoller", urlPatterns="/QnaBoardDetailContoller")
+public class QnaBoardDetailContoller extends HttpServlet {
 	
 	@Override
 	public void init(ServletConfig config) throws ServletException {
-		System.out.println("QnaBoardListContoller.init() 실행");
+		System.out.println("QnaBoardDetailContoller.init() 실행");
 	}
 	
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("QnaBoardListContoller.service() 실행");
+		System.out.println("QnaBoardDetailContoller.service() 실행");
 		
 		//JSP로 이동
-		request.getRequestDispatcher("/WEB-INF/views/homePage/qnaBoard/qnaBoardList.jsp").forward(request, response);
+		request.getRequestDispatcher("/WEB-INF/views/homePage/qnaBoard/qnaBoardDetail.jsp").forward(request, response);
 	}
 	
 	@Override
 	public void destroy() {
-		System.out.println("QnaBoardListContoller.destroy()");
+		System.out.println("QnaBoardDetailContoller.destroy()");
 	}
 }
 
--- src/controller/qnaBoard/QnaFrequentContoller.java
+++ src/controller/qnaBoard/QnaFrequentContoller.java
@@ -9,25 +9,25 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name="QnaBoardListContoller", urlPatterns="/QnaBoardListContoller")
-public class QnaBoardListContoller extends HttpServlet {
+@WebServlet(name="QnaFrequentContoller", urlPatterns="/QnaFrequentContoller")
+public class QnaFrequentContoller extends HttpServlet {
 	
 	@Override
 	public void init(ServletConfig config) throws ServletException {
-		System.out.println("QnaBoardListContoller.init() 실행");
+		System.out.println("QnaFrequentContoller.init() 실행");
 	}
 	
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("QnaBoardListContoller.service() 실행");
+		System.out.println("QnaFrequentContoller.service() 실행");
 		
 		//JSP로 이동
-		request.getRequestDispatcher("/WEB-INF/views/homePage/qnaBoard/qnaBoardList.jsp").forward(request, response);
+		request.getRequestDispatcher("/WEB-INF/views/homePage/qnaBoard/qnaFrequent.jsp").forward(request, response);
 	}
 	
 	@Override
 	public void destroy() {
-		System.out.println("QnaBoardListContoller.destroy()");
+		System.out.println("QnaFrequentContoller.destroy()");
 	}
 }
 
