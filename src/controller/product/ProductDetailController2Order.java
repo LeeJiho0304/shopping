1ce0ea1e7dc6527a777b5273e8915cbad4b92c6c
LemonGlacier <hijy02122@gmail.com>
2022-12-06 21:29:30 +0900

    20221206 productDetail-order 연결

---
+++ src/controller/product/ProductDetailController2Order.java
@@ -0,0 +1,33 @@
+package controller.product;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.ProductDAO;
+import dto.product.ProductDTO;
+import service.ProductService;
+
+@WebServlet(name="ProductDetailController2Order", urlPatterns="/ProductDetailController2Order")
+public class ProductDetailController2Order extends HttpServlet {
+	ProductDTO productDTO;
+	
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ServletContext application = request.getServletContext();
+		ProductService productService = (ProductService)application.getAttribute("productContentService");
+		//productDTO = productService.getContent(50);
+		
+		request.setAttribute("productDTO", productDTO);
+		
+		request.getRequestDispatcher("/WEB-INF/views/homePage/product/productDetail-Order.jsp").forward(request, response);
+	}
+	
+}
